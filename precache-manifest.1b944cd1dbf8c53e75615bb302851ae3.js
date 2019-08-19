5f9df496f63ee54c8f87d7adce2db42b298b801e
SeanZhu <seanzhuwx@gmail.com>
2019-08-19 14:48:23 +1000

    Updates

---
+++ precache-manifest.1b944cd1dbf8c53e75615bb302851ae3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1255feb23337f1c4cbb21ad90fa62e9",
+    "url": "/diet-routine/index.html"
+  },
+  {
+    "revision": "8836e88759fbc45ce781",
+    "url": "/diet-routine/static/css/main.1f3400e6.chunk.css"
+  },
+  {
+    "revision": "7b23420c1893b0be8a48",
+    "url": "/diet-routine/static/js/2.6217fb2d.chunk.js"
+  },
+  {
+    "revision": "8836e88759fbc45ce781",
+    "url": "/diet-routine/static/js/main.5b5d1ca4.chunk.js"
+  },
+  {
+    "revision": "7eea375228d3829c1cb9",
+    "url": "/diet-routine/static/js/runtime~main.f976091a.js"
+  }
+]);
